32dd03b12633d53043ff9c45408abf979a5a3bbc
Pritam Makwana <pritammakwana17561@gmail.com>
2025-09-27 17:41:30 +0530

    Install Vuex and create store

---
+++ backend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hello from './components/HelloWorld.vue'
+</script>
+
+<template>
+<div>
+<Hello />
+</div>
+</template>
+
+<style scoped></style>
---
+++ backend/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {ref, computed} from 'vue'
+import store from '../store'
+
+const test = computed(() => store.state.test)
+
+</script>
+
+<template>
+    <div>
+    <h1>Hello</h1>
+    <h1>{{ test }}</h1>
+    </div>
+</template>
