7e36ecc048b6506405542a4c89af27827aa93ffd
Pritam Makwana <pritammakwana17561@gmail.com>
2025-09-27 15:43:59 +0530

    migration add

--- database/migrations/2025_09_27_064848_create_customer_addresses_table.php
+++ database/migrations/2025_09_27_064848_create_customer_addresses_table.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+
     /**
      * Run the migrations.
      */
@@ -13,7 +14,16 @@ return new class extends Migration
     {
         Schema::create('customer_addresses', function (Blueprint $table) {
             $table->id();
+            $table->string('type',45);
+            $table->string('address1',255);
+            $table->string('address2',255)->nullable();
+            $table->string('city',255);
+            $table->string('state',45)->nullable();
+            $table->string('zipcode',45)->nullable();
+            $table->string('country_code',3);
+            $table->foreignId('customer_id')->references('id')->on('customers');
             $table->timestamps();
+            $table->foreign('country_code')->references('code')->on('countries');
         });
     }
 
